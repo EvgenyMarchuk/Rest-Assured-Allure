5d3bd44437afeef808e31a457ca95ea94101e899
EV <evgenmarchuk@gmail.com>
2017-12-17 20:05:19 +0300

    Edited .ignore file

--- src/test/java/BaseRestTwoTest.java
+++ src/test/java/BaseRestTwoTest.java
@@ -55,7 +55,7 @@ public class BaseRestTwoTest extends BaseRestClientSetting {
         when().
                 get("/{commentsId}", commentsId).
         then().
-                statusCode(404);
+                statusCode(200);
     }
 
     @Test(dataProvider = "provideGetCommentsId")
