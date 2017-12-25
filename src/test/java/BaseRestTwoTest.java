cdb34c29ceb60ff6b40ebb5cc203f8bea91e225e
EV <evgenmarchuk@gmail.com>
2017-12-25 20:06:29 +0300

    Added the fourth test

--- src/test/java/BaseRestTwoTest.java
+++ src/test/java/BaseRestTwoTest.java
@@ -11,7 +11,6 @@ import static io.restassured.http.ContentType.JSON;
 @Feature("This is a Feature Two")
 public class BaseRestTwoTest extends BaseRestClientSetting {
 
-    //New commit to the release brunch
     @BeforeClass
     public void SetUpClass(){
         RestAssured.basePath = "/comments";
