72391dcb426dec6eb548dacb0a68129c2a4a94d2
EV <evgenmarchuk@gmail.com>
2018-01-08 18:01:30 +0300

    Added helpers method

--- src/main/Java/Helpers/DataProviderClass.java
+++ src/main/Java/Helpers/DataProviderClass.java
@@ -6,11 +6,11 @@ public class DataProviderClass {
 
     @DataProvider(name = "provideGetUserId", parallel = true)
     public static Object[][] getUserId(){
-        return new Object[][]{{2000}, {2001}, {2002}, {2003}, {2004}, {2005}, {2006}};
+        return new Object[][]{{2000}, {2001}, {2002}, {2003}, {2004}, {2005}, {2006}, {2007}, {2008}};
     }
 
     @DataProvider(name = "provideGetCommentsId", parallel = true)
     public static Object[][] getCommentId(){
-        return new Object[][]{{2000}, {2001}, {2002}, {2003}, {2004}, {2005}, {2006}};
+        return new Object[][]{{2000}, {2001}, {2002}, {2003}, {2004}, {2005}, {2006}, {2007}, {2008}};
     }
 }
