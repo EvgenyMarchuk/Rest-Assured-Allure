75289682632cd0f8a8fb1db49a92d11d6a3808b7
EV <evgenmarchuk@gmail.com>
2018-01-14 13:31:12 +0300

    Added filter test

---
+++ src/test/java/PostsTest/FilterPostsTest.java
@@ -0,0 +1,30 @@
+package PostsTest;
+
+import Base.BasePosts;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasItems;
+
+public class FilterPostsTest extends BasePosts {
+
+    @Test
+    public void GetPostsUsingFilterIdTest(){
+        int firstId = 1;
+        int lastId = 5;
+
+        given().
+                contentType(JSON).
+                param("id", firstId).
+                param("id", lastId).
+        when().
+                get().
+        then().
+                statusCode(200).
+                body("id", hasItems(firstId, lastId));
+
+
+    }
+
+}
