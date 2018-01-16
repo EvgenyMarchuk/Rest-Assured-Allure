b2b3ff0d017ea26537135e82ba9b404f550c9eb8
EV <evgenmarchuk@gmail.com>
2018-01-16 23:09:25 +0300

    Added embed test

---
+++ src/test/java/UserTest/UserEmbedTest.java
@@ -0,0 +1,26 @@
+package UserTest;
+
+import Base.BaseUser;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class UserEmbedTest extends BaseUser {
+
+
+    @Test
+    public void UserEmbedToDosTest(){
+        int userId = 2;
+        given().
+                contentType(JSON).
+                param("_embed", "todos").
+        when().
+                get("/{userId}", userId).
+        then().
+                statusCode(200).
+                body("todos.userId", everyItem(is(userId))).
+                body("todos.id", everyItem(greaterThanOrEqualTo(1)));
+    }
+}
